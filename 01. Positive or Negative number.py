12a57f9b049fecbe6f5c9d3e55e5acf5bf4b6cb1
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-05 22:16:59 +0530

    Create 01. Positive or Negative number.py

---
+++ 01. Positive or Negative number.py
@@ -0,0 +1,11 @@
+Link : https://prepinsta.com/python-program/find-whether-a-given-number-is-positive-or-negative/
+
+def check(n):
+    if n == 0:
+        print("Zero")
+    elif n > 0:
+        print("Positive")
+    else:
+        print("Negative")
+
+check(2)
