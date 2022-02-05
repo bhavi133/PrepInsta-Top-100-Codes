6e137fa56697de54f7cb4e28ca020462bd866627
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-05 22:18:43 +0530

    Create 2. Even or Odd number.py

---
+++ 2. Even or Odd number.py
@@ -0,0 +1,10 @@
+Link : https://prepinsta.com/python-program/Check-Whether-a-Number-is-Even-or-Odd/
+
+def check(n):
+    if n % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+n = int(input("Input a number : "))
+check(n)
