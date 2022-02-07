daa3f4d10718a40267d922952effd5fd31be50d2
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-07 23:36:17 +0530

    Create 19. Power of a number.py

---
+++ 19. Power of a number.py
@@ -0,0 +1,8 @@
+Link : https://prepinsta.com/python-program/find-power-of-a-given-number/
+
+def power(base, expo):
+    return base ** expo
+
+base = int(input("Input a base number : "))
+expo = int(input("Input a exponential number : "))
+print(power(base, expo))
