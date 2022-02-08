417d89a5e7fd0caa8d6a81edf1ebcb88250991d9
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-08 21:01:19 +0530

    Create 21. Finding Prime Factors of a number.py

---
+++ 21. Finding Prime Factors of a number.py
@@ -0,0 +1,16 @@
+Link : https://prepinsta.com/python-program/prime-factors-of-a-number/
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+print(prime_factors(210))
