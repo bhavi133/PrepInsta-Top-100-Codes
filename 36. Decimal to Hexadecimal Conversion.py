d0403fd3b682baa981b0b672933e91f6b949ca1b
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-14 14:50:12 +0530

    Create 36. Decimal to Hexadecimal Conversion.py

---
+++ 36. Decimal to Hexadecimal Conversion.py
@@ -0,0 +1,5 @@
+Link : https://prepinsta.com/python-program/decimal-to-hexadecimal-conversion/
+
+dec_num = 2545
+hex_num = hex(dec_num)
+print("Conversion of decimal to hexadecimal : ", str(hex_num.upper()))
