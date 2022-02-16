dbb40cbf0b8318877448698d4c63545e6ecfd9e4
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-16 14:56:20 +0530

    Create 33. Hexadecimal to Decimal Conversion.py

---
+++ 33. Hexadecimal to Decimal Conversion.py
@@ -0,0 +1,5 @@
+Link :https://prepinsta.com/python-program/hexadecimal-to-decimal-conversion/
+
+hex_num = "0xff"
+dec_num = int(hex_num, 16)
+print("Conversion of hexadecimal to decimal : ", str(dec_num))
