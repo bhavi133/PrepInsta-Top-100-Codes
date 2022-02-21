4c834713675a6d2fa4f41afb246f38a9e9a84ee0
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-21 15:54:35 +0530

    Create 46. Check whether a character is a vowel or consonant.py

---
+++ 46. Check whether a character is a vowel or consonant.py
@@ -0,0 +1,8 @@
+Link : https://prepinsta.com/python-program/for-checking-a-character-is-a-vowel-or-consonant/
+
+char = input("Input a character : ")
+vowels = list('aeiouAEIOU')
+if char in vowels:
+    print("Character is vowel")
+else:
+    print("Character is consonant")
