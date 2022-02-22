721030566fb2e30d04da5c4fe6b65368d5573a7c
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-22 13:57:18 +0530

    Create 48. Calculate the area of a circle.py

---
+++ 48. Calculate the area of a circle.py
@@ -0,0 +1,6 @@
+Link : https://prepinsta.com/python-program/calculating-the-area-of-circle/
+
+from math import pi
+
+radius = float(input("Input a radius of a circle : "))
+print("The area of circle is : ", pi*radius*radius)
