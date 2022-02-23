d7aced23f9180774c510d8b1692f1da6a66616b9
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-23 15:12:54 +0530

    Create 51. Calculate the number of digits in an integer.py

---
+++ 51. Calculate the number of digits in an integer.py
@@ -0,0 +1,8 @@
+Link : https://prepinsta.com/python-program/to-calculate-number-of-digits-in-an-integer/
+
+num = int(input("Input a number : "))
+s = str(num)
+ctr = 0
+for i in s:
+    ctr += 1
+print(int(ctr))
