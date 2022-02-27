b27d71c3f085ede74232570818ffdeb8f46af73b
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-27 12:33:48 +0530

    Create 62. Reverse an array.py

---
+++ 62. Reverse an array.py
@@ -0,0 +1,7 @@
+Link : https://prepinsta.com/python-program/python-code-for-reverse-an-array/
+
+def reverse_array(arr):
+    return arr[::-1]
+
+arr = [10, 89, 9, 56, 4, 80, 8]
+print(reverse_array(arr))
