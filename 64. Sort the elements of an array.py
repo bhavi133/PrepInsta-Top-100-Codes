fd83c3386115e45666e3fb360c90c5add7b0e757
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-28 12:43:06 +0530

    Create 64. Sort the elements of an array.py

---
+++ 64. Sort the elements of an array.py
@@ -0,0 +1,5 @@
+Link : https://prepinsta.com/python-program/to-sort-the-elements-of-an-array/
+
+arr = [10, 89, 9, 56, 4, 80, 8, -1]
+print("In ascending order : ", sorted(arr))
+print("In descending order : ", sorted(arr, reverse=True))
