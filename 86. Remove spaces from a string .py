44c34120290c624f4c40a7f30cdcde5e1a917891
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-03-03 12:52:11 +0530

    Create 86. Remove spaces from a string .py

---
+++ 86. Remove spaces from a string .py
@@ -0,0 +1,7 @@
+Link : https://prepinsta.com/python-program/remove-spaces-from-a-string/
+
+def remove_spaces(string):
+    return ("".join(string.split()))
+
+string = "Prep Insta"
+print(remove_spaces(string))
