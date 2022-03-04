82662ad5f6c74998c1d470ac2c5fe8e7b44eea86
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-03-04 12:07:23 +0530

    Create 87. Remove brackets from an algebraic expression .py

---
+++ 87. Remove brackets from an algebraic expression .py
@@ -0,0 +1,10 @@
+Link : https://prepinsta.com/python-program/to-remove-brackets-from-an-algebraic-expression/
+
+def remove_brackets(expression):
+    str = ""
+    for i in expression:
+        if ord(i) == 41 or ord(i) == 40 or ord(i) == 91 or ord(i) == 93 or ord(i) == 123 or ord(i) == 125:
+            pass
+        else:    
+            str += i
+    return str
