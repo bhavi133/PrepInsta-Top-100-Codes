160b57ecec856b4da0559ed9b3fb7491c87546ac
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-12-20 20:16:48 +0530

    Update 14. Armstrong number.py

--- 14. Armstrong number.py
+++ 14. Armstrong number.py
@@ -1,5 +1,6 @@
 Link : https://prepinsta.com/python-program/to-find-a-number-is-armstrong-or-not/
 
+Code 1 :
 num = int(input("Input a number : "))
 sum = 0
 n = num
@@ -12,3 +13,16 @@ if num == sum:
     print(f"{num} is an armstrong number")
 else:
     print(f"{num} is not an armstrong number")
+
+Code 2 :
+    
+def armstrongNumber(n):
+    l1 = list(map(int, str(n)))
+    y = len(l1)
+    l2 = list(map(lambda x:x**y, l1))
+    if(sum(l2) == n):
+        return 'Yes'
+    else:
+        return 'No'
+
+print(armstrongNumber(1634))
